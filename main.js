77e511a3d4c356b10360ffd134f1afabf69087d5
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-04-06 21:07:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ loadDatabase()
 // if (opts['cluster']) {
 //   require('./lib/cluster').Cluster()
 // }
-global.authFile = `${opts._[0] || 'sesinya'}.json`
+global.authFile = `${opts._[0] || 'antieror'}.json`
 global.isInit = !fs.existsSync(authFile)
 const { state, saveState } = useSingleFileAuthState(global.authFile)
 
