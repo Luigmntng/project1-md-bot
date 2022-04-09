63679e43cd000b706d095dfa4ba700a971cd2b1a
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-04-10 00:37:32 +0700

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,1 +1,15 @@
-let fetch = require("node-fetch")let handler = async (m, { conn }) => {let res = await fetch('https://raw.githubusercontent.com/VideFrelan/words/main/ptl.txt')let txt = await res.text()let arr = txt.split('\n')let cita = arr[Math.floor(Math.random() * arr.length)]conn.sendFile(m.chat, cita, 'asupan.mp4', 'Nih kak asupan nya!!', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }}) }handler.tags = ['video']handler.help = ['asupan']handler.command = /^(asupan)$/ihandler.limit = truemodule.exports = handler
+let fetch = require("node-fetch")
+let arr = []
+let handler = async (m, { conn }) => {
+  let res = await fetch('http://sansekai.my.id/sansekai.txt')
+  if (!res.ok) throw `${res.status} ${res.statusText}`
+let txt = await res.text()
+arr = txt.split`\n`
+  let asupan = arr[Math.floor(Math.random() * arr.length)]
+  await conn.sendFile(m.chat, 'http://sansekai.my.id/ptl_repost/'+asupan, asupan, null, m)
+}
+handler.help = ['asupan']
+handler.tags = ['fun']
+handler.command = /^(asupan)$/i
+
+module.exports = handler
