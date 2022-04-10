5cd2b3d27bbae347b4662a61133b4a77e260822c
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-04-10 11:49:18 +0700

    Update logoneko.js

--- plugins/logoneko.js
+++ plugins/logoneko.js
@@ -1,13 +1,16 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan text1|text2\n\ncontoh: .logoneko elyas|ganzz'
-  m.reply('*Wait ngab*\nProses...')
-  let res = `https://caliphapi.com/api/girlneko?text=${response[0]}&text2=${response[1]}&apikey=ELYASXD`
-  conn.sendFile(m.chat, res, 'nekologo.jpg', `Sudah jadi`, m, false)
+  if (!args[0]) throw 'Masukkan Parameter'
+  m.reply('Proses...')
+  let res = `https://hardianto.xyz/api/bot/gfx4?apikey=hardianto&text1=${response[0]}&text2=${response[1]}`
+  conn.sendFile(m.chat, res, 'neko.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['logoneko'].map(v => v + ' <text|text>')
 handler.tags = ['maker']
 handler.command = /^(logoneko)$/i
 
 module.exports = handler
+
+
+
