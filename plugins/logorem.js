640fa3428b294cb456846ca1f70799d4e238896d
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-04-10 11:51:01 +0700

    Update logorem.js

--- plugins/logorem.js
+++ plugins/logorem.js
@@ -1,15 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-   response = args.join(' ')
-  if (!args[0]) throw 'Masukkan Text'
-  m.reply('*Wait ngab*\nProses...')
-  let res = `https://caliphapi.com/api/rem?text=${response}&text2=Elyas&apikey=ELYASXD`
-  conn.sendFile(m.chat, res, 'kaneki.jpg', `Sudah jadi`, m, false)
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Parameter'
+  m.reply('_Proses..._')
+  let res = `https://hardianto.xyz/api/bot/gfx5?apikey=hardianto&text=${response[0]}`
+  conn.sendFile(m.chat, res, 'gura.jpg', `Nih kak`, m, false)
 }
-handler.help = ['logorem'].map(v => v + ' <teks>')
+handler.help = ['logorem'].map(v => v + ' <text>')
 handler.tags = ['maker']
 handler.command = /^(logorem)$/i
-handler.limit = true
-handler.register = false
 
 module.exports = handler
